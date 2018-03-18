782ceb437a9e44081d36444f0b2e2967c9e84bc2
Ryan Thames <ryanthames@gmail.com>
2018-03-18 16:59:35 -0500

    Generate unordered seating chart

--- src/main/java/com/ocsoftware/teachertools/SeatingChartGenerator.java
+++ src/main/java/com/ocsoftware/teachertools/SeatingChartGenerator.java
@@ -7,6 +7,12 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class SeatingChartGenerator {
+  private static final Integer[] seats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+  private static final String[] rows = {"B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"};
+
+  private static int currentSeatIndex = 0;
+  private static int currentRowIndex = 0;
+
   private static final Map<Award, Person> awards = new LinkedHashMap<>();
   private static final Map<Person, String> seatingChart = new LinkedHashMap<>();
   private static final Set<Person> people = new LinkedHashSet<>();
@@ -16,23 +22,47 @@ public class SeatingChartGenerator {
     initSeatingChart();
     generateSeatingChart();
     // TODO: 3/17/18 generating output based on criteria
+
+    seatingChart.forEach((p, s) -> System.out.println(String.format("%s | %s", p.toString(), s)));
   }
 
   private static void generateSeatingChart() {
     List<Person> ssStudents = people.stream().filter(Person::isSs).collect(Collectors.toList());
     List<Person> nhsStudents = people.stream().filter(Person::isNhs).collect(Collectors.toList());
-    List<Person> remaningStudents = people.stream().filter(p -> !p.isSs() && !p.isNhs()).collect(Collectors.toList());
+    List<Person> remainingStudents = people.stream().filter(p -> !p.isSs() && !p.isNhs()).collect(Collectors.toList());
 
     Map<Person, Integer> awardCount = generateAwardCountMap();
 
-    List<Person> multipleAwardWinners = remaningStudents.stream().filter(p -> awardCount.get(p) > 1).collect(Collectors.toList());
-    List<Person> singleAwardWinners = remaningStudents.stream().filter(p -> awardCount.get(p) == 1).collect(Collectors.toList());
+    List<Person> multipleAwardWinners = remainingStudents.stream().filter(p -> awardCount.get(p) > 1).collect(Collectors.toList());
+    List<Person> singleAwardWinners = remainingStudents.stream().filter(p -> awardCount.get(p) == 1).collect(Collectors.toList());
+
+    // assign ss and nhs students
+    ssStudents.forEach(s -> seatingChart.put(s, "Row A"));
+    nhsStudents.forEach(s -> seatingChart.put(s, "Stage"));
+
+    multipleAwardWinners.forEach(SeatingChartGenerator::assignSeat);
+    singleAwardWinners.forEach(SeatingChartGenerator::assignSeat);
 
     // TODO: 3/18/18 handle teachers and blanks
     // TODO: 3/18/18 handle sorting between different buckets of students
     // TODO: 3/18/18 handle missing students
+  }
 
-    int x = 5;
+  private static void assignSeat(Person s) {
+    validateIndex();
+    seatingChart.put(s, String.format("%s%d", rows[currentRowIndex], seats[currentSeatIndex]));
+    currentSeatIndex++;
+  }
+
+  private static void validateIndex() {
+    if (currentSeatIndex >= seats.length) {
+      resetIndices();
+    }
+  }
+
+  private static void resetIndices() {
+    currentRowIndex++;
+    currentSeatIndex = 0;
   }
 
   private static void initSeatingChart() {
@@ -77,13 +107,13 @@ public class SeatingChartGenerator {
 
     Person existingPerson = getPerson(person);
 
-    if(existingPerson != null) {
+    if (existingPerson != null) {
       person = existingPerson;
     } else {
       people.add(person);
     }
 
-    if("Special Services".equals(award.getCategory())) {
+    if ("Special Services".equals(award.getCategory())) {
       person.setSs(true);
     }
 
@@ -93,7 +123,7 @@ public class SeatingChartGenerator {
   private static Person getPerson(Person person) {
     List<Person> result = people.stream().filter(p -> p.equals(person)).collect(Collectors.toList());
 
-    if(result != null && result.size() > 0) {
+    if (result != null && result.size() > 0) {
       // Should only be one result
       return result.get(0);
     } else {
@@ -107,7 +137,7 @@ public class SeatingChartGenerator {
     awards.forEach((a, p) -> {
       Integer count = awardCount.get(p);
 
-      if(count == null) {
+      if (count == null) {
         awardCount.put(p, 1);
       } else {
         awardCount.put(p, ++count);
